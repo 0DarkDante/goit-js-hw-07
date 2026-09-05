eddeede1b4e01d9334858d115997ff728cb7a9ee
Кулинич Вадим <vadik@MacBook-Air-Kulinic.local>
2026-09-05 21:53:53 +0300

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+let form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    if (form.elements.email.value.trim() === '' || form.elements.password.value.trim() === '') {
+        alert('All form fields must be filled in');
+        return;
+    }
+
+    let obj = {
+        email: form.elements.email.value.trim(),
+        password: form.elements.password.value.trim()
+    }
+
+    console.log(obj);
+    form.reset();
+})
