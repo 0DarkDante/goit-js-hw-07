f5bb91481e304c93544247732fd7d11bda474ed9
Кулинич Вадим <vadik@MacBook-Air-Kulinic.local>
2026-09-05 22:23:40 +0300

    task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+let btn = document.querySelector('.change-color'),
+    color = document.querySelector('.color');
+
+btn.addEventListener('click', event => {
+  let randomColor = getRandomHexColor();
+  document.body.style.backgroundColor = randomColor;
+  color.textContent = randomColor;
+});
